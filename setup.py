09485be4af61081244bf4e03ebf63d6cf5e1564d
Mikhail Itkin <itkin.m@gmail.com>
2023-03-21 23:18:09 +0100

    add scripts to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/loniitkina/sidrift",
     packages=setuptools.find_packages(include=["sidrift"]),
+    scripts=["bin/sidtrack"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
